41c12af202bb36a058858205ab39c3e5ac15f451
Ohad Ravid <ohad.rv@gmail.com>
2018-04-12 15:46:28 +0200

    Initial Commit

---
+++ Nag WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  Nag WatchKit Extension
+//
+//  Created by Ohad Ravid on 12/04/2018.
+//  Copyright © 2018 Ravid Ventures. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
