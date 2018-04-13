2e0956560df9730643769e20fb6bd92a01cdc871
Ohad Ravid <ohad.rv@gmail.com>
2018-04-13 21:03:17 +0200

    Big red circle button

--- Nag WatchKit Extension/NGSingleTimerInterfaceController.swift
+++ Nag WatchKit Extension/NGSingleTimerInterfaceController.swift
@@ -9,17 +9,46 @@
 import WatchKit
 import Foundation
 
+extension TimeInterval {
+    private var milliseconds: Int {
+        return Int((truncatingRemainder(dividingBy: 1)) * 1000)
+    }
+    
+    private var seconds: Int {
+        return Int(self) % 60
+    }
+    
+    private var minutes: Int {
+        return (Int(self) / 60 ) % 60
+    }
+    
+    private var hours: Int {
+        return Int(self) / 3600
+    }
+    
+    var stringTime: String {
+        if hours != 0 {
+            return "\(hours)h \(minutes)m \(seconds)s"
+        } else if minutes != 0 {
+            return "\(minutes)m \(seconds)s"
+        } else if milliseconds != 0 {
+            return "\(seconds)s \(milliseconds)ms"
+        } else {
+            return "\(seconds)s"
+        }
+    }
+}
 
 class NGSingleTimerInterfaceController: WKInterfaceController {
     @IBOutlet var timerName: WKInterfaceLabel!
-    @IBOutlet var timerTimeRemaining: WKInterfaceTimer!
+    @IBOutlet var timerTimeRemaining: WKInterfaceLabel!
     
     var timer: NGTimer? {
         didSet {
             guard let timer = timer else { return }
             
             timerName.setText(timer.title)
-            timerTimeRemaining.setDate(Date(timeIntervalSinceNow: timer.duration + 1 ))
+            timerTimeRemaining.setText(timer.duration.stringTime)
         }
     }
     
