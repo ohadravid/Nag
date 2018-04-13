59e142e1e0473b360273b58f68e6f5702ae14946
Ohad Ravid <ohad.rv@gmail.com>
2018-04-13 20:32:50 +0200

    Added controller for singe timer windows

---
+++ Nag WatchKit Extension/NGSingleTimerInterfaceController.swift
@@ -0,0 +1,33 @@
+//
+//  NGSingleTimerInterfaceController.swift
+//  Nag WatchKit Extension
+//
+//  Created by Ohad Ravid on 13/04/2018.
+//  Copyright © 2018 Ravid Ventures. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class NGSingleTimerInterfaceController: WKInterfaceController {
+    @IBOutlet var timerName: WKInterfaceLabel!
+    @IBOutlet var timerTimeRemaining: WKInterfaceTimer!
+    
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
